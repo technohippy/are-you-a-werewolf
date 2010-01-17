a1213116e542eb520102bcffaa18d096628b21c5
andyjpn <andyjpn@6429617e-0381-11df-96c3-55d8896d8cd1>
2010-01-17 18:37:01 +0000

    send wave to tell what his role

--- models.py
+++ models.py
@@ -7,8 +7,12 @@ from google.appengine.ext.db import polymodel
 
 class Game(db.Model):
   wave_id = db.StringProperty()
+  wolves_wave_id = db.StringProperty()
+  fortuneteller_wave_id = db.StringProperty()
+  hunter_wave_id = db.StringProperty()
 
 class Participant(db.Model):
+  personal_wave_id = db.StringProperty()
   participant_id = db.StringProperty()
   role = db.StringProperty(default='villager')
   alive = db.BooleanProperty(default=True)
